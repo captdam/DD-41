a5351f067f9052a3db71b315c961edd1b35bec72
Captdam <captdam@beardle.com>
2019-10-15 02:45:26 -0400

    Update ppm.c

--- ROV/MainController/MainController/ppm.c
+++ ROV/MainController/MainController/ppm.c
@@ -11,7 +11,7 @@
 // Some ESCs or servos accept PPM_ZONE 1000, this vary from manufactures. Test it first.
 
 #define PPM_MAXDIFF_MAIN	PPM_ZONE/0.5
-#define PPM_MAXDIFF_AUX		PPM_ZONE/16
+#define PPM_MAXDIFF_AUX		PPM_ZONE/16.0
 
 // PPM generator init
 void initPPM() {
@@ -19,14 +19,14 @@ void initPPM() {
 	TCCR1A = (2<<COM1A0) | (2<<COM1B0) | (2<<WGM10); //Mode 14: Fast PWM, set at zero, clear on compare match
 	TCCR1B = (3<<WGM12) | (2<<CS10); //Clk scaler = 8 => PWM resolution = 0.5us, range = 32.8ms
 	ICR1 = 39999; //OF every 20ms
-	OCR1A = 3000; //Allow range 3000 +/- 1000
+	OCR1A = 3000; //Allow range PPM_MAXDIFF_MAIN
 	OCR1B = 3000; //Default position at center
 	
 	//Setup PPM2 and PPM3
 	TIMSK1 = (1<<TOIE1); //When T1 overflow (reaches ICR1), software-ly set the pin
 	TCCR0A = (2<<COM0A0) | (2<<COM0B0); //Mode 0: Free running timer. When T0 reaches the compare value, software-ly clear the pin
 	TCCR0B = (4<<CS00); //Clk scaler = 256 => PWM resolution = 16us, range = 4.1ms. A 128 scaler will be better, but the highest scaler under 256 is 64.
-	OCR0A = 94; //Allow range 94 +/- 31.5
+	OCR0A = 94; //Allow range 94 +/- PPM_MAXDIFF_AUX
 	OCR0B = 94;
 	
 	/*
